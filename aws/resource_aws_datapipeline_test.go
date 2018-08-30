39081c03d3f831dd68b3bb087b64416bc5d1241b
kterada0509 <kterada.0509sg@gmail.com>
2018-08-30 23:29:32 +0900

    wip

--- aws/resource_aws_datapipeline_test.go
+++ aws/resource_aws_datapipeline_test.go
@@ -67,6 +67,39 @@ func testSweepDataPipeline(region string) error {
 
 func TestAccAWSDataPipeline_basic(t *testing.T) {
 
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSDataPipelineDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAWSDataPipelineConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAWSDataPipelineExists("aws_db_instance.bar"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "allocated_storage", "10"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "engine", "mysql"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "license_model", "general-public-license"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "instance_class", "db.t2.micro"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "name", "baz"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "username", "foo"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "parameter_group_name", "default.mysql5.6"),
+					resource.TestCheckResourceAttr(
+						"aws_datapipeline.bar", "enabled_cloudwatch_logs_exports.#", "0"),
+					resource.TestCheckResourceAttrSet("aws_datapipeline.bar", "hosted_zone_id"),
+					resource.TestCheckResourceAttrSet("aws_datapipeline.bar", "ca_cert_identifier"),
+					resource.TestCheckResourceAttrSet(
+						"aws_datapipeline.bar", "resource_id"),
+				),
+			},
+		},
+	})
 }
 
 func testAccCheckAWSDataPipelineDestroy(s *terraform.State) error {
@@ -109,3 +142,43 @@ func testAccCheckAWSDataPipelineDestroy(s *terraform.State) error {
 
 	return nil
 }
+
+func testAccCheckAWSDataPipelineExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No DB Instance ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*AWSClient).datapipelineconn
+
+		opts := datapipeline.DescribePipelinesInput{
+			PipelineIds: []*string{aws.String(rs.Primary.ID)},
+		}
+
+		resp, err := conn.DescribePipelines(&opts)
+
+		if err != nil {
+			return err
+		}
+
+		if len(resp.PipelineDescriptionList) != 1 ||
+			*resp.PipelineDescriptionList[0].PipelineId != rs.Primary.ID {
+			return fmt.Errorf("DataPipeline not found")
+		}
+
+		return nil
+	}
+}
+
+// DataPipeline names cannot collide, and deletion takes so long, that making the
+// name a bit random helps so able we can kill a test that's just waiting for a
+// delete and not be blocked on kicking off another one.
+var testAccAWSDataPipelineConfig = `
+resource "aws_datapipeline" "bar" {
+
+}`
